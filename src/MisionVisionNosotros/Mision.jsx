821fcc934bd82bb94103a56dac5e33bb1bb7447c
javier mosquera <andrubjj@gmail.com>
2023-12-13 17:05:33 -0500

    subida Miercoles 13/12/2023 (7 cambios 17:05)

--- src/MisionVisionNosotros/Mision.jsx
+++ src/MisionVisionNosotros/Mision.jsx
@@ -3,12 +3,15 @@ import niños from "../image/doctora/NIÑOS.jpg"
 import "../Components/Estilos/Home.css"
 
 function Mision() {
-  return (    
-    <div id='diseñoMision'>
+  return (  
+    <>
       <img src={niños} alt="" id='imagenMision' />
-        <h1 id='mision'>MISION</h1>
-        <p id='pMision'>Ser el centro de apoyo terapeutico en Bogotá y Calí más reconocido por su labor social en calidad y amor terapeutico a toda la población que lo requiera.</p>
-    </div>
+      <div id='diseñoMision'>
+          <h1 id='mision'>MISION</h1>
+          <p id='pMision'>Ser el centro de apoyo terapeutico en Bogotá y Calí más reconocido por su labor social en calidad y amor terapeutico a toda la población que lo requiera.</p>
+      </div>
+    
+    </>  
     
   )
 }
--- src/MisionVisionNosotros/ValoracionCompañia.jsx
+++ src/MisionVisionNosotros/ValoracionCompañia.jsx
@@ -1,15 +1,28 @@
 import React from 'react'
+import doctoaDibujo from "../image/mision vision/doctoradibujo.jpeg"
 import "../Components/Estilos/Home.css"
+import { Col, Row } from 'react-bootstrap'
 
 function Valoracion() {
   return (
     <div>
       <h1 className='tituloValoresCompañia'>VALORES DE LA COMPAÑIA</h1>
-      <p className='parrafoValoresCompañia'>Profesionalismo y empatia.</p>
-      <p className='parrafoValoresCompañia'>Integridad e innovación.</p>
-      <p className='parrafoValoresCompañia'>Compromiso y responsabilidad.</p>
-      <p className='parrafoValoresCompañia'>Trabajo en equipo.</p>
+      <Row>
+        <Col className="col-md-4">
+          <img src={doctoaDibujo} alt="" id='imagenMision' />
+        </Col>
+        <Col>
+          <div>
+            <p className='parrafoValoresCompañia'>Profesionalismo y empatia.</p>
+            <p className='parrafoValoresCompañia'>Integridad e innovación.</p>
+            <p className='parrafoValoresCompañia'>Compromiso y responsabilidad.</p>
+            <p className='parrafoValoresCompañia'>Trabajo en equipo.</p>
+          </div>     
+        </Col>
+      </Row>
+    
     </div>
+    
   )
 }
 
